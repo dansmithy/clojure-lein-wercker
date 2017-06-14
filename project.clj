ac7f33fdd096a1e6080bb88af30ab23ec262ed71
Danny Smith <danny.j.smith@oracle.com>
2017-06-14 11:59:41 +0100

    Setting .m2 repo directory to either be local or be the WERCKER_CACHE_DIR.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler compojure-basic.handler/app}
+  :local-repo #=(eval (or (System/getenv "WERCKER_CACHE_DIR") "local-m2"))
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
